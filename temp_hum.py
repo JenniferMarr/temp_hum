c1a0a7110bbbeb01ed36f3fc53e3468d2b198e33
Jennifer Marroquin <jennifer.marroquin@correounivalle.edu.co>
2025-07-09 23:51:29 -0500

    cambios sql

--- temp_hum.py
+++ temp_hum.py
@@ -2,29 +2,43 @@ from grovepi import *
 from grove_rgb_lcd import *
 import time
 from math import isnan
+import sqlite3
+from datetime import datetime
 
-dth_sensor_port = 7
+
+dht_sensor_port = 7
+
+conn = sqlite3.connect('sensores.db')
+cursor = conn.cursor()
 
 while True:
+    print("Leyendo sensor...")
     try:
-        [ temp, hum ] = dht(dth_sensor_port, 1)
-        
+        [temp, hum] = dht(dht_sensor_port, 0)  
+        print("Resultado de dht():", [temp, hum])
+
         if not (isnan(temp) or isnan(hum)):
-            print("Temp =", temp, "C\tHumidity =", hum, "%")
-            
+            print("Temp =", temp, "°C   Humidity =", hum, "%")
+
             t = round(temp, 1)
             h = round(hum, 1)
-            
+
             setRGB(0, 255, 0)
-            setText("Temp:" + str(t) + "C    Humidity:" + str(h) + "%")
+            setText("Temp:" + str(t) + "C   " + "Humidity:" + str(h) + "%")
+
+            fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            cursor.execute("INSERT INTO lecturas (fecha, temperatura, humedad) VALUES (?, ?, ?)", (fecha, t, h))
+            conn.commit()
+            print("Datos guardados en la base de datos.")
+
         else:
-            print("Datos no válidos (NaN)")
+            print("Datos no validos (NaN)")
             setRGB(255, 0, 0)
             setText("Sensor error")
-            
+
     except (IOError, TypeError) as e:
-        print("Error", e)
+        print("Error:", e)
         setRGB(255, 0, 0)
         setText("Read error")
-    
+
     time.sleep(2)
